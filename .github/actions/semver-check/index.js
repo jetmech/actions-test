7073dfbbf5782c9ed1d0ea6d8fef044a0cdf6e64
jetmech <josephlesage84@gmail.com>
2022-09-26 21:50:01 +1000

    Update action

--- .github/actions/semver-check/index.js
+++ .github/actions/semver-check/index.js
@@ -1,9 +1,10 @@
 const github = require("@actions/github");
 const core = require("@actions/core");
 
-const context = { github };
+const context = github.context;
 
 async function run() {
+  core.debug(`The event is ${context.eventName}`);
   try {
     core.debug(new Date().toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
   } catch (error) {
