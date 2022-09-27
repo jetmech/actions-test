8dd072485b9b21fcb62fc6e1885cb1370e5ca404
jetmech <josephlesage84@gmail.com>
2022-09-27 17:31:22 +1000

    Look at context keys

--- .github/actions/semver-check/dist/index.js
+++ .github/actions/semver-check/dist/index.js
@@ -9672,6 +9672,10 @@ const context = github.context;
 
 async function run() {
   core.info(`The event is ${context.eventName}`);
+  if (typeof context === "object") {
+    core.info("Here are the context keys:");
+    Object.keys(context).forEach((key) => core.info(key));
+  }
   try {
     core.debug(new Date().toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
   } catch (error) {
