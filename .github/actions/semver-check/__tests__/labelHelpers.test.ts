91f50968fe719534c6a9c48d9b8b972a573548d6
jetmech <josephlesage84@gmail.com>
2022-10-02 18:53:11 +1100

    Finish labelHelpers

--- .github/actions/semver-check/__tests__/labelHelpers.test.ts
+++ .github/actions/semver-check/__tests__/labelHelpers.test.ts
@@ -1,9 +1,26 @@
+import { Context } from "@actions/github/lib/context";
 import {
   hasOnlyOneReleaseTypeLabel,
   getReleaseTypeFromLabels,
+  getLabelNames,
+  noReleaseLabelErrorMessage,
 } from "../src/labelHelpers";
 
-const testLabels = ["Version: prepatch alpha", "foo", "bar", "bar"];
+const testLabels = ["Version: prepatch alpha", "foo", "bar", "baz"];
+
+type LabeLike = { name: string };
+
+const createGithubContext = (labels: LabeLike[]) =>
+  ({
+    payload: {
+      pull_request: {
+        number: 42,
+        labels,
+      },
+    },
+  } as Partial<Context>);
+
+getLabelNames(createGithubContext([{ name: "foo" }]) as Context);
 
 describe(".hasOnlyOneReleaseTypeLabel()", () => {
   it("Returns true if the array of label names has only one release type label", () => {
@@ -18,13 +35,50 @@ describe(".hasOnlyOneReleaseTypeLabel()", () => {
 
     expect(result).toBe(false);
   });
+});
 
-  describe("getReleaseTypeFromLabels", () => {
-    it("extracts the release type and prerelease id as tuple [ReleaseType, prereleaseId]", () => {
-      const result = getReleaseTypeFromLabels(testLabels);
+describe(".getReleaseTypeFromLabels()", () => {
+  it("extracts the release type and prerelease id as tuple [ReleaseType, prereleaseId]", () => {
+    const result = getReleaseTypeFromLabels(testLabels);
 
-      const expected = ["prepatch", "alpha"];
-      expect(result).toEqual(expected);
-    });
+    const expected = ["prepatch", "alpha"];
+    expect(result).toEqual(expected);
+  });
+  it("the second value of the ReleaseAndPrereleaseId tuple is undefined if no prerelease id is found in the label", () => {
+    const testLabels = ["Version: patch"];
+    const result = getReleaseTypeFromLabels(testLabels);
+
+    const expected = ["patch", undefined];
+    expect(result).toEqual(expected);
+  });
+  it("throws an error if there are no release type labels", () => {
+    const testLabels = ["foo"];
+
+    const shouldThrow = () => {
+      getReleaseTypeFromLabels(testLabels);
+    };
+
+    expect(shouldThrow).toThrow(noReleaseLabelErrorMessage);
+  });
+});
+
+describe(".getLabelNames()", () => {
+  it("returns an array of labes names when passed the GitHub context", () => {
+    const labels = [{ name: "foo" }, { name: "bar" }];
+    const context = createGithubContext(labels);
+
+    const expectedLabelNames = labels.map((label) => label.name);
+
+    expect(getLabelNames(context as Context)).toEqual(expectedLabelNames);
+  });
+
+  it("throws an error if the pull request is not labeled", () => {
+    const context = createGithubContext([]);
+
+    const shouldThrow = () => {
+      getLabelNames(context as Context);
+    };
+
+    expect(shouldThrow).toThrow("The pull request has no labels");
   });
 });
--- .github/actions/semver-check/src/labelHelpers.ts
+++ .github/actions/semver-check/src/labelHelpers.ts
@@ -3,7 +3,7 @@ import { Label } from "@octokit/webhooks-types";
 import { ReleaseType } from "semver";
 
 type AllowedReleaseTypes = (ReleaseType | "no change")[];
-export type ReleaseAndPrereleaseId = [ReleaseTypeLabel, string];
+export type ReleaseAndPrereleaseId = [ReleaseTypeLabel, string | undefined];
 
 const allowedReleaseTypes: AllowedReleaseTypes = [
   "patch",
@@ -44,29 +44,6 @@ function isLabelArray(labels: unknown[]): labels is Label[] {
 }
 
 /**
- * Extract the release type and prerelease id from the label.
- * @param label
- * @returns A tuple containing the release type and prerelease id (if provided).
- */
-export function extractReleaseTypeFromLabel(
-  label: string
-): ReleaseAndPrereleaseId {
-  const match = label.match(labelRegex);
-
-  if (!match) {
-    throw Error("Oh no!");
-  }
-
-  const [, version, prereleaseId] = match;
-
-  if (isApprovedReleaseType(version)) {
-    return [version, prereleaseId];
-  } else {
-    throw Error(`Unable to extract release type from label: ${label}`);
-  }
-}
-
-/**
  * Determine if the pull request has exactly one release type label.
  * @param labelNames An array of pull request label names.
  * @returns True if the array of pull request labels has only on release type label.
@@ -90,25 +67,62 @@ export const hasOnlyOneReleaseTypeLabel = (labelNames: string[]) => {
  */
 export const getLabelNames = (context: Context) => {
   const labels = context.payload.pull_request?.labels;
+  let labelNames: string[] = [];
 
   if (isLabelArray(labels)) {
-    return labels.map((label) => label.name);
+    labelNames = labels.map((label) => label.name);
+  }
+
+  if (labelNames.length === 0) {
+    throw Error("The pull request has no labels.");
   } else {
-    throw Error("Pull request has no labels");
+    return labelNames;
   }
 };
+
+const createReleaseTypeList = () =>
+  allowedReleaseTypes
+    .map(
+      (releaseType) =>
+        `  - Version: ${releaseType}${
+          releaseType.startsWith("pre") ? " prereleaseId" : ""
+        }`
+    )
+    .join("\n");
+
+export const noReleaseLabelErrorMessage = `No release label found.
+
+The pull request requires one of the following
+labels determined by your intended release type:
+${createReleaseTypeList()}
+Note: The prereleaseId is optional and is only used for
+prerelease, prepatch, preminor and premajor release types.
+
+For example, you may have an alpha, beta or rc prerelease id.
+You can label the pull request with "Version: premajor alpha"
+to facilitate an alpha release, etc...
+
+This action is built using https://www.npmjs.com/package/semver.
+Please refer to the link above for notes on npm semantic versioning.
+`;
 
 /**
  * Get the release type and prerelease id from the pull request label.
  * @param labels An array of labels from the pull request.
  * @returns A tuple containing the release type and the prerelease id.
  */
-export const getReleaseTypeFromLabels = (labels: string[]) => {
+export const getReleaseTypeFromLabels = (
+  labels: string[]
+): ReleaseAndPrereleaseId => {
   for (const label of labels) {
-    if (labelRegex.test(label)) {
-      return extractReleaseTypeFromLabel(label);
+    const match = label.match(labelRegex);
+    if (match) {
+      const [, releaseType, prereleaseId] = match;
+      if (isApprovedReleaseType(releaseType)) {
+        return [releaseType, prereleaseId];
+      }
     }
   }
 
-  throw Error("No semver label found");
+  throw Error(noReleaseLabelErrorMessage);
 };
