e1f686be88d933bee1cec903d351ba567aeb2e92
jetmech <josephlesage84@gmail.com>
2022-10-05 12:28:04 +1100

    Add summary

--- .github/actions/tester/dist/index.js
+++ .github/actions/tester/dist/index.js
@@ -9531,6 +9531,7 @@ const core = __importStar(__nccwpck_require__(2186));
 const createListFromObjectKeys = (obj) => Object.keys(obj)
     .map((key) => `- ${key}`)
     .join("\n");
+const getKeys = (obj) => Object.keys(obj).map((key) => `- ${key}`);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -9538,6 +9539,13 @@ function run() {
             core.info(createListFromObjectKeys(process.env));
             core.info("## The following are available on the github context:");
             core.info(createListFromObjectKeys(github.context));
+            core.summary
+                .addHeading("The following are environment variables:", 2)
+                .addList(getKeys(process.env))
+                .addBreak()
+                .addHeading("The following are available on the github context:", 2)
+                .addList(getKeys(github.context))
+                .write();
         }
         catch (error) {
             if (error instanceof Error)
--- .github/actions/tester/src/index.ts
+++ .github/actions/tester/src/index.ts
@@ -6,6 +6,8 @@ const createListFromObjectKeys = (obj: {}) =>
     .map((key) => `- ${key}`)
     .join("\n");
 
+const getKeys = (obj: {}) => Object.keys(obj).map((key) => `- ${key}`);
+
 async function run() {
   try {
     core.info("## The following are environment variables:");
@@ -13,6 +15,14 @@ async function run() {
 
     core.info("## The following are available on the github context:");
     core.info(createListFromObjectKeys(github.context));
+
+    core.summary
+      .addHeading("The following are environment variables:", 2)
+      .addList(getKeys(process.env))
+      .addBreak()
+      .addHeading("The following are available on the github context:", 2)
+      .addList(getKeys(github.context))
+      .write();
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
