bb3ce0c1b310b4eaee80751b108715f60bea24cb
jetmech <josephlesage84@gmail.com>
2022-10-05 11:50:33 +1100

    Add dist

--- .github/actions/tester/src/index.ts
+++ .github/actions/tester/src/index.ts
@@ -1,11 +1,18 @@
 import * as github from "@actions/github";
 import * as core from "@actions/core";
 
-const context = github.context;
-const { GITHUB_WORKSPACE } = process.env;
+const createListFromObjectKeys = (obj: {}) =>
+  Object.keys(obj)
+    .map((key) => `- ${key}`)
+    .join("\n");
 
 async function run() {
   try {
+    core.info("## The following are environment variables:");
+    core.info(createListFromObjectKeys(process.env));
+
+    core.info("## The following are available on the github context:");
+    core.info(createListFromObjectKeys(github.context));
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
